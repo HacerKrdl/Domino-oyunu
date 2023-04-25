65471442c27ecb0a49e73a785dc3025ce1da1c79
Hacer Karadöl <103882655+HacerKrdl@users.noreply.github.com>
2023-04-25 23:17:54 +0300

    Add files via upload

---
+++ DominoÖdev/DominoÖdev/Class1.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DominoÖdev
+{
+    class Class1
+    {
+    }
+    interface IYetenek
+    {
+
+    }
+    public class Dominoes
+    {
+        public string oyuncu1;
+        public string oyuncu2;
+        private int tasDegeri;
+        private string Tas;
+
+        public Dominoes()
+        {
+            List<Dominoes> dominoes = new List<Dominoes>();
+            string[] tasDegeri = {
+            "0,1","0,2","0,3","0,4","0,5","0,6",
+            "1,2","1,3","1,4","1,5","1,6",
+            "2,3","2,4","2,5","2,6",
+            "3,4","3,5","3,6",
+            "4,5","4,6",
+            "5,6",
+            "0,0","1,1","2,2", "3,3", "4,4","5,5","6,6"
+            };
+
+        }
+        
+    }
+}
---
+++ DominoÖdev/DominoÖdev/Form1.cs
@@ -0,0 +1,123 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace DominoÖdev
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            this.Text = "Domino Oyunu";
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            label3.Text = "Taşları dağıtıldı.";
+            int sayi = listBox1.Items.Count;
+            int[] taslar = new int[sayi];
+            Random rnd = new Random();
+            int i = 0;
+            while (i < 7)
+            {
+                int secim = rnd.Next(0, sayi);
+                if (Array.IndexOf(taslar, secim) == -1)
+                {
+                    taslar[i] = secim;
+                    comboBox1.Text += listBox1.Items[secim - 1] + "\n";
+                    i++;
+                }
+            }
+            Random rnd1 = new Random();
+            int j = 0;
+            while (j < 7)
+            {
+                int secim1 = rnd1.Next(0, sayi);
+                if (Array.IndexOf(taslar, secim1) == -1)
+                {
+                    taslar[j] = secim1;
+                    comboBox2.Text += listBox1.Items[secim1 - 1] + "\n";
+                    j++;
+                }
+            }
+            Random rnd2= new Random();
+            int k = 0;
+            while (k < 14)
+            {
+                int secim2 = rnd2.Next(0, sayi);
+                if (Array.IndexOf(taslar, secim2) == -1)
+                {
+                    taslar[k] = secim2;
+                    comboBox3.Text += listBox1.Items[secim2 - 1] + "\n";
+                    k++;
+                }
+            }
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            label1.Text = "Taş ortaya eklendi.";
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            label2.Text = "Taş çekildi.";
+        }
+
+        private void comboBox1_SelectedIndexChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void comboBox2_SelectedIndexChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void textBox1_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void comboBox3_SelectedIndexChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void textBox2_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void textBox3_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button4_Click(object sender, EventArgs e)
+        {
+            string ad1 = textBox2.Text;
+            label9.Text = ad1;
+            string ad2 = textBox2.Text;
+            label11.Text = ad2;
+            label12.Text = "İsimler Kaydedildi.";
+        }
+
+        private void label12_Click(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
